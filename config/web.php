4144ec895f04c22fde14783d958fc5cd32172047
陈庆龙 <cql@sxw100.com>
2017-10-28 18:25:22 +0800

    去掉无用注释

--- config/web.php
+++ config/web.php
@@ -15,7 +15,6 @@ $config = [
 	'defaultRoute' => 'home/show',
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'rcBExitruWfddacxeReU9kAZ9Kf8XiGp',
         ],
         'cache' => [
